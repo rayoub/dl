c19c531db57bb8d186e4e1d79c7c9051b0c975f8
Ronald Ayoub <ronaldayoub@outlook.com>
2019-05-18 18:49:38 -0500

    increment

--- py/fibo.py
+++ py/fibo.py
@@ -2,34 +2,36 @@
 # TODO
 # 1. saving model
 # 2. loading model
-# 3. missing data
+# 3. padding
 # 4. optimization 
 # 5. regularization
 # 6. validation data
 
 import tensorflow as tf
+import metrics as mets
 
 # *******************************************
 # *** INPUT PIPELINE ***
 # *******************************************
 
 BATCH_SIZE = 32
+TIME_STEPS = 121
 VAL_SIZE = 512
 BUFFER_SIZE = 20000
 MAX_EPOCHS = 40
 
 # one-hot lookups
-MAP_AA_KEYS = ['_','A','B','C','D','E','F','G','H','I','K','L','M','N','P','Q','R','S','T','V','W','X','Y','Z']
+MAP_AA_KEYS = ['A','B','C','D','E','F','G','H','J','I','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 MAP_AA_VALS = list(range(len(MAP_AA_KEYS)))
 MAP_AA_VALS_CNT = len(MAP_AA_VALS)
 MAP_AA_INIT = tf.lookup.KeyValueTensorInitializer(MAP_AA_KEYS, MAP_AA_VALS, value_dtype=tf.int64)
-MAP_AA = tf.lookup.StaticVocabularyTable(MAP_AA_INIT, 1)
+MAP_AA = tf.lookup.StaticHashTable(MAP_AA_INIT, -1)
 
-MAP_SS_KEYS = ['_','G','H','I','B','E','C','S','T']
-MAP_SS_VALS = [ 0 , 1 , 1 , 1 , 2 , 2 , 3 , 3 , 3 ]
+MAP_SS_KEYS = ['G','H','I','B','E','C','S','T']
+MAP_SS_VALS = [ 0 , 0 , 0 , 1 , 1 , 2 , 2 , 2 ]
 MAP_SS_VALS_CNT = len(set(MAP_SS_VALS))
 MAP_SS_INIT = tf.lookup.KeyValueTensorInitializer(MAP_SS_KEYS, MAP_SS_VALS, value_dtype=tf.int64)
-MAP_SS = tf.lookup.StaticVocabularyTable(MAP_SS_INIT, 1)
+MAP_SS = tf.lookup.StaticHashTable(MAP_SS_INIT, -1)
 
 # mapping functions
 def map_aa (elem):
@@ -57,27 +59,32 @@ def map_aa_ss_batch (elem):
     return c
 
 # run input pipeline
-ds = tf.data.TextLineDataset(['aa_ss.txt'])
+ds = tf.data.TextLineDataset(['train.txt'])
 val_ds = ds.take(VAL_SIZE).batch(BATCH_SIZE, drop_remainder=False).map(map_aa_ss_batch)
-train_ds = ds.skip(VAL_SIZE).shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True).map(map_aa_ss_batch)
+test_ds = ds.skip(VAL_SIZE).take(VAL_SIZE).batch(BATCH_SIZE, drop_remainder=False).map(map_aa_ss_batch)
+train_ds = ds.skip(2 * VAL_SIZE).shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True).map(map_aa_ss_batch)
 
 # *******************************************
 # *** MODEL DEFINITION ***
 # *******************************************
 
-# define loss function
-def loss(labels, logits):
-    return tf.losses.categorical_crossentropy(labels, logits, from_logits=True) 
-
 # define the model
 model = tf.keras.Sequential([
     tf.keras.layers.Dense(128, batch_input_shape=(BATCH_SIZE, None, MAP_AA_VALS_CNT)),
-    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(200, return_sequences=True, stateful=False, recurrent_initializer='glorot_uniform')),
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(256, return_sequences=True, stateful=False, recurrent_initializer='glorot_uniform')),
     tf.keras.layers.Dense(MAP_SS_VALS_CNT)
 ])
 
+# define loss function
+def loss(labels, logits):
+
+    # this will return zero for zero vector labels representing missing data
+    return tf.keras.losses.categorical_crossentropy(labels, logits, from_logits=True)
+
 # compile the model
-model.compile(optimizer='adam', loss=loss, metrics=['categorical_accuracy'] )
+model.compile(optimizer='adam', 
+        loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True, reduction=tf.keras.losses.Reduction.SUM), 
+        metrics=[mets.CategoricalAccuracyWithMissingData(BATCH_SIZE,TIME_STEPS)])
 
 # *******************************************
 # *** TRAINING LOOP ***
@@ -85,12 +92,14 @@ model.compile(optimizer='adam', loss=loss, metrics=['categorical_accuracy'] )
 
 callbacks = [
         # early stopping
-        tf.keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0.001, patience=3, verbose=0, restore_best_weights=True)
+        tf.keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0.001, patience=4, verbose=0, restore_best_weights=True)
     ]
 
 model.fit(train_ds, epochs=MAX_EPOCHS, validation_data=val_ds, callbacks=callbacks)
 
 
+# to look at prediction 
+# p = tf.math.argmax(model.predict(test_ds), axis=-1)
 
 
 
