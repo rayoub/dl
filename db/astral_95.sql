985d739c38cee2d73e6582514bf1d4c50f46e05c
Ronald Ayoub <ronaldayoub@outlook.com>
2019-11-08 08:38:58 -0600

    astral 95 reps

---
+++ db/astral_95.sql
@@ -0,0 +1,6 @@
+
+CREATE TABLE astral_95
+(
+    scop_id VARCHAR NOT NULL
+);
+
---
+++ db/x_astral_95.sql
@@ -0,0 +1,8 @@
+
+TRUNCATE astral_95;
+
+COPY astral_95 (scop_id) FROM '/home/ayoub/git/dl/train/astral-95-2.07.txt' WITH (DELIMITER ',');
+
+
+
+
