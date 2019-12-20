a7fb08a636a86ff68b56b68020d74424a1f5b492
Ronald Ayoub <ronaldayoub@outlook.com>
2019-12-20 06:31:19 -0600

    refine counts

--- db/x_gram_counts.sql
+++ db/x_gram_counts.sql
@@ -1,35 +1,47 @@
 
 CREATE TABLE gram_counts AS
-WITH counts AS
+WITH basic_counts AS
 (
     SELECT
         residue_code_1,
         residue_code_2, 
         residue_code_3,
         descriptor,
-        COUNT(*) AS count
+        COUNT(*) AS basic_count
     FROM
         valid_gram
     GROUP BY 
         CUBE(residue_code_1, residue_code_2, residue_code_3, descriptor)
+),
+group_counts AS
+(
+    SELECT
+        '_'
+        ||
+        CASE WHEN residue_code_1 IS NOT NULL THEN 'r1' ELSE '' END 
+        ||
+        CASE WHEN residue_code_2 IS NOT NULL THEN 'r2' ELSE '' END 
+        ||
+        CASE WHEN residue_code_3 IS NOT NULL THEN 'r3' ELSE '' END 
+        ||
+        CASE WHEN descriptor IS NOT NULL THEN 'd' ELSE '' END AS group_set,
+        residue_code_1,
+        residue_code_2,
+        residue_code_3,
+        descriptor,
+        basic_count AS group_count
+    FROM
+        basic_counts
 )
 SELECT
-    '_'
-    ||
-    CASE WHEN residue_code_1 IS NOT NULL THEN 'r1' ELSE '' END 
-    ||
-    CASE WHEN residue_code_2 IS NOT NULL THEN 'r2' ELSE '' END 
-    ||
-    CASE WHEN residue_code_3 IS NOT NULL THEN 'r3' ELSE '' END 
-    ||
-    CASE WHEN descriptor IS NOT NULL THEN 'd' ELSE '' END AS grouping_set,
+    group_set,
     residue_code_1,
     residue_code_2,
     residue_code_3,
     descriptor,
-    count
+    group_count
 FROM
-    counts;
+    group_counts;
 
 
 
